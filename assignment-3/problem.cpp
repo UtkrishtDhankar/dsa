d0be2b0a403c6025be1036c349ee55ca3f4c316b
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-03-19 15:32:10 +0530

    Wrote a solution to the problem number 5

--- assignment-3/problem.cpp
+++ assignment-3/problem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "queue.hpp"
 
@@ -12,6 +13,25 @@ int main(int argc, char* argv[])
     std::cout << usage_str;
     return 1;
   }
+
+  int num = std::stoi(argv[1]);
+  int rule = std::stoi(argv[2]);
+
+  cs202::queue<int> q;
+
+  for (int i = 0; i < num; i++) {
+    q.push(num - i);
+  }
+
+  for (int i = 1; i < num; i++) {
+    for (int j = 1; j < rule; j++) {
+      q.push(q.pop());
+    }
+
+    q.pop();
+  }
+
+  std::cout << q.pop() << std::endl;
 
   return 0;
 }
