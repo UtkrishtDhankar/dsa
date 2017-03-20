f1651667e15a26511947cb0ed111861606c35e22
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-03-20 12:38:36 +0530

    Commented the problem solving source

--- assignment-3/problem.cpp
+++ assignment-3/problem.cpp
@@ -9,6 +9,7 @@ int main(int argc, char* argv[])
     "NUMBER\t the number of children in the game.\n"
     "RULE\t the elimination rule\n";
 
+  // If the argument pattern isn't good, then print usage
   if (argc != 3) {
     std::cout << usage_str;
     return 1;
@@ -17,12 +18,13 @@ int main(int argc, char* argv[])
   int num = std::stoi(argv[1]);
   int rule = std::stoi(argv[2]);
 
+  // Create the queue
   cs202::queue<int> q;
-
   for (int i = 0; i < num; i++) {
     q.push(num - i);
   }
 
+  // Simulate the problem
   for (int i = 1; i < num; i++) {
     for (int j = 1; j < rule; j++) {
       q.push(q.pop());
