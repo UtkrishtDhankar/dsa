d12082cd49d5e83b79210f1b46f8019f4f23cff0
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-03-27 20:10:23 +0530

    Got DoubleHashMap to work

--- assignment-4/DoubleHashMap.hpp
+++ assignment-4/DoubleHashMap.hpp
@@ -7,40 +7,104 @@
 #ifndef DOUBLEHASHMAP_HPP_
 #define DOUBLEHASHMAP_HPP_
 
+#include <stdexcept>
+
+#include "Dictionary.hpp"
+#include "include/list.hpp"
+#include "include/seq_linear_list.hpp"
 
 namespace cs202
 {
 template<class Key, class Value>
+struct DoubleHashMapData {
+  DoubleHashMapData() {
+    key = Key(); value = Value();
+    deleted = false;
+    filled = false;
+  }
+
+  DoubleHashMapData(const DoubleHashMapData<Key, Value>& other) {
+    key = other.key;
+    value = other.value;
+    deleted = other.deleted;
+    filled = other.filled;
+  }
+
+  DoubleHashMapData(const Key& k, const Value& v) : key(k), value(v) {
+    deleted = false;
+    filled = false;
+  }
+
+  bool operator==(const DoubleHashMapData<Key, Value>& other) const {
+    return (key == other.key && value == other.value && deleted = other.deleted && filled = other.filled);
+  }
+
+  Key key;
+  Value value;
+  bool deleted;
+  bool filled;
+};
+
+template<class Key, class Value>
 class DoubleHashMap  : public Dictionary<Key,Value>
 {
+
+  static constexpr size_t default_size = 101;
+  LinearList<DoubleHashMapData<Key, Value> > map;
+  hash<Key> key_hash;
+  hash<Key> offset_hash;
+
+  unsigned int num_filled;
     /*
-     * Function rehash:
+     * Function rehash
      * Resizes the has table to the next convenient size.
      * Called when all the slots are full and a new element needs to be inserted.
      */
-	void rehash();
+	void rehash() {
+    // TODO
+  }
+
+  // Returns the location for a key
+  int loc_for_key(Key key) const {
+    return key_hash(key) % map.capacity();
+  }
+
+  int get_offset(Key key) const {
+    return offset_hash(key) % map.capacity();
+  }
 public:
     /*
      * Constructor: DoubleHashMap
      * Creates a Double Hash Hash Table with some default size.
      * NOTE: Please try to ensure that the size is a prime number for better performance.
      */
-	DoubleHashMap();
+	DoubleHashMap(hash<Key> hash_func_key, hash<Key> hash_func_offset)
+    : map(default_size, DoubleHashMapData<Key, Value> ()), key_hash(hash_func_key), offset_hash(hash_func_offset)
+  {
+    num_filled = 0;
+  }
     /*
      * Constructor:DoubleHashMap
      * Creates an empty Double Hash Map with the ability to hold atleast num Key value pairs.
      */
-	DoubleHashMap(const int& num);
+	DoubleHashMap(const int& num, hash<Key> hash_func_key, hash<Key> hash_func_offset)
+    : map(num, DoubleHashMapData<Key, Value> ()), key_hash(hash_func_key), offset_hash(hash_func_offset)
+  {
+    num_filled = 0;
+  }
+
     /*
      * Constructor: Double Hash Map
      * Creates a new Hash Table which is the exact copy of the given hash table.
      **/
 	DoubleHashMap(DoubleHashMap<Key, Value>& ht);
+  // TODO implement this
+
     /*
      * Destructor
      * Deletes the memory acquired by the given Hash Map.
      */
-	~DoubleHashMap();
+	~DoubleHashMap() {}
     /*
      * A convenience wrapper operator.
      * Returns a reference to the value corresponding to the given key.
@@ -52,8 +116,101 @@ public:
      * ht[1] = 4;
      * ht[2] = 3;
      */
-	Value& operator[](const Key& key);
+  Value& operator[](const Key& key) {
+    if (!has(key)) {
+      put(key, Value());
+    }
+
+    return get(key);
+  }
+
+    /*
+     * Function : has
+     * Returns true if the dictionay contains the key
+     * false otherwise. It is search operation
+     */
+	virtual bool has(const Key& key) {
+    bool found = false;
+
+    int num_checked = 0;
+    int i = loc_for_key(key);
+    for(; num_checked < map.capacity(); i = (i + get_offset(key)) % map.capacity(), num_checked++) {
+      if (map[i].key == key && map[i].filled && !map[i].deleted) {
+        found  = true;
+        break; // We've found a spot to put it in
+      }
+    }
+
+    return found;
+  }
+
+  /*
+   * Function : remove
+   * Removes the given key and the corresponding value from the Dictionary if the key is in the dictionary.
+   * Does nothing otherwise.
+   */
+	virtual void remove(const Key& key) {
+    // TODO
+  }
+
+  /*
+   * Function : get
+   * Returns the value associated with the given key.
+   * Raises an exception if the key does'nt exist in the dictionary.
+   */
+	virtual Value& get(const Key& key) {
+    bool found = false;
+
+    // int i = loc_for_key(key);
+    int i = 0;
+    int num_checked = 0;
+
+    for(; num_checked < map.capacity(); i = (i + get_offset(key)) % map.capacity(), num_checked++) {
+      if (map[i].key == key && map[i].filled && !map[i].deleted) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      throw std::invalid_argument("Key not found in dictionary.");
+    }
+
+    return map[i].value;
+  }
+
+  /*
+   * Function : put
+   * If the key does'nt exist in the dictionary, then insert the given key and value in the dictionary.
+   * Otherwise change the value associated with the key to the given value.
+   */
+	virtual void put(const Key& key, const Value& value) {
+    int num_checked = 0;
+
+    if (num_filled == map.capacity()) {
+      rehash();
+    }
+
+    int i = loc_for_key(key);
+    for(; num_checked < map.capacity(); i = (i + get_offset(key)) % map.capacity(), num_checked++) {
+      if (map[i].key == key && map[i].filled && !map[i].deleted) {
+        map[i].value = value;
+        return; // We're done
+      } if (!map[i].filled && !map[i].deleted) {
+        break; // We've found a spot to put it in
+      }
+    }
+
+    // If we're here, then we've found a spot to put the thing in
+    map[i].key = key;
+    map[i].value = value;
+    map[i].filled = true;
+    num_filled++;
+  }
 };
+
+template<class Key, class Value>
+constexpr size_t DoubleHashMap<Key, Value>::default_size;
 }
 
-#endif 
+#endif
---
+++ assignment-4/DoubleHashMapDriver.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+#include "DoubleHashMap.hpp"
+
+template<>
+struct cs202::hash<int> {
+  int m;
+
+  hash(int m_in) {m = m_in;}
+
+  unsigned long long operator() (int k) const {
+    return (unsigned long long) k % m;
+  }
+};
+
+int main()
+{
+  cs202::hash<int> hash_func(101);
+  cs202::hash<int> offset_func(5);
+  cs202::DoubleHashMap<int, int> map(hash_func, offset_func);
+
+  std::string command;
+  while (std::cin >> command) {
+    if (command == "put") {
+      int key;
+      std::cin >> key;
+
+      int val;
+      std::cin >> val;
+
+      map.put(key, val);
+    } else if (command == "get") {
+      int key;
+      std::cin >> key;
+
+      std::cout << map.get(key) << std::endl;
+    } else if (command == "braces") {
+      int key;
+      std::cin >> key;
+
+      int val;
+      std::cin >> val;
+
+      map[key] = val;
+    } else if (command == "has") {
+      int key;
+      std::cin >> key;
+
+      std::cout << map.has(key) << std::endl;
+    } else if (command == "remove") {
+      int key;
+      std::cin >> key;
+
+      map.remove(key);
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
