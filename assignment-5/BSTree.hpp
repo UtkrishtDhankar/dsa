be5638d7438dfbc3370c007ed49b115993622a6d
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-04-09 10:42:56 +0530

    Got a basic BSTree driver going. Also fixed errors in given interface for BST

--- assignment-5/BSTree.hpp
+++ assignment-5/BSTree.hpp
@@ -1,7 +1,10 @@
 #ifndef BSTREE_HPP
 #define BSTREE_HPP 1
 #include "BinaryTree.hpp"
-template <Key, Value>
+
+namespace cs202 {
+
+template <class Key, class Value>
 class BSTree : public BinaryTree<Key, Value> {
 
 /* Inherit as many functions as possible from BinaryTree.
@@ -9,19 +12,29 @@ class BSTree : public BinaryTree<Key, Value> {
  * Also make sure that all inherited functions work correctly in the context of a binary search tree.
  */
  public:
-  /*
-   * This method returns the current height of the binary search tree.
-   */
-  virtual int getHeight();
-  /*
-   * This method returns the total number of elements in the binary search tree.
-   */
-  virtual int size();
-  /*
-   * This method prints the key value pairs of the binary search tree, sorted by
-   * their keys.
-   */
-  virtual void print();
+    /*
+    * This method returns the current height of the binary search tree.
+    */
+    virtual int getHeight() {
+        // TODO implement
+        return 0;
+    }
+    /*
+    * This method returns the total number of elements in the binary search tree.
+    */
+    virtual int size() {
+        // TODO implement
+        return 0;
+    }
+    /*
+    * This method prints the key value pairs of the binary search tree, sorted by
+    * their keys.
+    */
+    virtual void print() {
+        // TODO implement
+    }
+};
+
 }
 
 #endif /* ifndef BSTREE_HPP */
---
+++ assignment-5/BSTreeDriver.cpp
@@ -0,0 +1,67 @@
+#include "BSTree.hpp"
+
+#include <iostream>
+#include <string>
+
+int main() {
+    cs202::BSTree<int, int> bt;
+
+    std::string command;
+    while (std::cin >> command) {
+        if (command == "put") {
+            int key;
+            std::cin >> key;
+
+            int val;
+            std::cin >> val;
+
+            bt.put(key, val);
+
+        } else if (command == "has") {
+            int key;
+            std::cin >> key;
+
+            std::cout << bt.has(key) << std::endl;
+        } else if (command == "get") {
+            int key;
+            std::cin >> key;
+
+            std::cout << bt.get(key) << std::endl;
+        } else if (command == "remove") {
+            int key;
+            std::cin >> key;
+
+            bt.remove(key);
+        } else if (command == "preorder") {
+            bt.print_pre_order();
+        } else if (command == "inorder") {
+            bt.print_in_order();
+        } else if (command == "postorder") {
+            bt.print_post_order();
+        } else if (command == "print") {
+            bt.print();
+        } else if (command == "max") {
+            std::cout << bt.maximum() << std::endl;
+        } else if (command == "min") {
+            std::cout << bt.minimum() << std::endl;
+        } else if (command == "height") {
+            std::cout << bt.getHeight() << std::endl;
+        } else if (command == "size") {
+            std::cout << bt.size() << std::endl;
+        } else if (command == "successor") {
+            int key;
+            std::cin >> key;
+
+            std::cout << bt.successor(key) << std::endl;
+        } else if (command == "predecessor") {
+            int key;
+            std::cin >> key;
+
+            std::cout << bt.predecessor(key) << std::endl;
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
