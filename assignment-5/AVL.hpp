96e5d897b44babe346a5fc34a9adb91f0ef2afbe
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-04-15 12:28:41 +0530

    Got insertion to work in AVL, still no fixup though

--- assignment-5/AVL.hpp
+++ assignment-5/AVL.hpp
@@ -4,6 +4,27 @@
 
 namespace cs202 {
 
+template<class Key, class Value>
+class AVLNode : public BinaryNode<Key, Value> {
+public:
+    Key key;
+    Value val;
+    AVLNode<Key,Value> * root, * left, * right, * parent;
+
+    int height;
+
+    AVLNode() {
+        height = 0;
+    }
+
+    AVLNode(Key in_key, Value in_value,
+                AVLNode<Key, Value>* r, AVLNode<Key, Value>* p)
+          : key(in_key), val(in_value), root(r), parent(p) {
+        left = right = nullptr;
+        height = 0;
+    }
+};
+
 template <class Key, class Value>
 class AVL : public BSTree<Key, Value> {
 /*
@@ -23,6 +44,66 @@ class AVL : public BSTree<Key, Value> {
  * some flexibility in how you implement the internals of your AVL Tree. But
  * make sure these functions are there.
  */
+private:
+    void increase_height_up_tree(AVLNode<Key, Value>* node) {
+        while (node->parent) {
+            AVLNode<Key, Value>* parent = node->parent;
+            if (parent->left && parent->right) {
+                // Set the parent's height
+                parent->height = 1 
+                    + parent->left->height > parent->right->height 
+                    ? parent->left->height : parent->right->height;
+            } else if (parent->left) {
+                parent->height = 1 + parent->left->height;
+            } else if (parent->right) {
+                parent->height = 1 + parent->right->height;
+            } else {
+                // we really shouldn't be here
+                // TODO throw exception here
+            }
+
+            node = parent;
+        }
+    }
+
+protected:
+    AVLNode<Key, Value>* root;
+
+    virtual AVLNode<Key, Value>* put_under_node(AVLNode<Key, Value>* node, const Key& key, const Value& value) {
+        if (key == node->key) {
+            node->val = value;
+            return node;
+        } else if (key > node->key) {
+            if (node->right) {
+                return put_under_node(node->right, key, value);
+            } else {
+                node->right = new AVLNode<Key, Value> (key, value, this->root, node);
+                return node->right;
+            }
+        } else if (key < node->key) {
+            if (node->left) {
+                return put_under_node(node->left, key, value);
+            } else {
+                node->left = new AVLNode<Key, Value> (key, value, this->root, node);
+                return node->left;
+            }
+        } else {
+            return nullptr; // something has gone wrong if this happens
+        }
+    }
+
+public:
+    virtual void put(const Key& key, const Value& value) override {
+        if (!root) {
+            root = new AVLNode<Key, Value>(key, value, nullptr, nullptr);
+            root->root = root;
+            root->height = 0;
+        } else {
+            AVLNode<Key, Value>* inserted_node = put_under_node(root, key, value);
+            increase_height_up_tree(inserted_node);
+        }
+    }
+
 };
 
 }
