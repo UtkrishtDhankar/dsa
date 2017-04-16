6681cf0871e69f7c6edd9ac956f0dcce235794a0
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-04-16 17:56:19 +0530

    There's some problems with height not updating, specifically in the case where we're adding elements in increasing order, but otherwise AVL's done

--- assignment-5/AVL.hpp
+++ assignment-5/AVL.hpp
@@ -27,23 +27,7 @@ public:
 
 template <class Key, class Value>
 class AVL : public BSTree<Key, Value> {
-/*
- * Inherit as much functionality as possible from the BSTree class.
- * Then provide custom AVL Tree functionality on top of that.
- * The AVL Tree should make use of as many BST functions as possible.
- * Override only those methods which are extremely necessary.
- */
-/*
- * Apart from that, your code should have the following four functions:
- * getBalance(node) -> To get the balance at any given node;
- * doBalance(node) -> To fix the balance at the given node;
- * rightRotate(node) -> Perform a right rotation about the given node.
- * leftRotate(node) -> Perform a left rotation about the given node.
- *
- * The signature of these functions are not provided in order to provide you
- * some flexibility in how you implement the internals of your AVL Tree. But
- * make sure these functions are there.
- */
+
 private:
     int max(int a, int b) {
         return a > b ? a : b;
@@ -142,9 +126,6 @@ protected:
                     }
                     this->right_rotation_at(cur);
 
-                    // cur->height -= 2;
-                    // left->height--;
-                    // cur->get_parent()->height++;
                     fix_height_of_node(cur);
                     fix_height_of_node(left);
                     fix_height_of_node(cur->get_parent());
@@ -154,8 +135,6 @@ protected:
                 if (compute_balance_factor(right) == -1) {
                     this->left_rotation_at(cur);
 
-                    // cur->height -= 2;
-                    // right->height--;
                     fix_height_of_node(cur);
                     fix_height_of_node(right);
                 } else {
@@ -164,13 +143,12 @@ protected:
                     }
                     this->left_rotation_at(cur);
 
-                    // cur->height -= 2;
-                    // right->height--;
-                    // cur->get_parent()->height++;
                     fix_height_of_node(cur);
                     fix_height_of_node(right);
                     fix_height_of_node(cur->get_parent());
                 }
+            } else {
+                fix_height_of_node(cur);
             }
         }
     }
@@ -187,7 +165,6 @@ public:
             AVLNode<Key, Value>* inserted_node = put_under_node(this->root, key, value);
             increase_height_up_tree(inserted_node);
             fixup_at(inserted_node);
-            increase_height_up_tree(inserted_node);
         }
     }
 
@@ -258,7 +235,6 @@ public:
 
     virtual int getHeight() override {
         AVLNode<Key, Value>* avl_root = dynamic_cast<AVLNode<Key, Value>* > (this->root);
-        avl_root->height = this->height_under_node(avl_root);
 
         return avl_root->height;
     }
