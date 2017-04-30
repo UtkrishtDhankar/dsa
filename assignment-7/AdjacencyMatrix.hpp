b8432074c39faa66988a7ed6f8307b3c104cac11
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-04-30 09:53:04 +0530

    Got AdjacencyMatrix to work!

--- assignment-7/AdjacencyMatrix.hpp
+++ assignment-7/AdjacencyMatrix.hpp
@@ -1,7 +1,119 @@
 #ifndef ADJACENCY_MATRIX
 #define ADJACENCY_MATRIX 1
+
+#include <stdexcept>
+
 #include "GraphAdjacencyBase.hpp"
+#include "include/list.hpp"
+#include "include/seq_linear_list.hpp"
+
+namespace cs202 {
+
 class AdjacencyMatrix : public GraphAdjacencyBase {
-  ~AdjacencyMatrix();
+private:
+	LinearList<int> matrix;
+	int v;
+
+	bool is_valid_index(int i, int j) const {
+		return (i < v && j < v);
+	}
+
+	int index_of(int i, int j) const {
+		return v * i + j;
+	}
+
+	int& get(int i, int j) {
+		if (is_valid_index(i, j)) {
+			return matrix[index_of(i, j)];
+		} else {
+			throw std::invalid_argument("Invalid indices to matrix.");
+		}
+	}
+
+	const int& const_get(int i, int j) const {
+		if (is_valid_index(i, j)) {
+			return matrix.at(index_of(i, j));
+		} else {
+			throw std::invalid_argument("Invalid indices to matrix.");
+		}
+	}
+
+
+public:
+	AdjacencyMatrix(int in_v) : matrix(in_v * in_v, 0), v(in_v) {
+
+	}
+
+	/* Destructor:
+	 * releases all resources acquired by the class
+	 */
+	virtual ~AdjacencyMatrix() {
+		// no need to do anything, the matrix will delete itself
+	}
+	/*
+	 * Function: edgeExists
+	 * Returns true if an edge exists between vertices i and j, false otherwise.
+	 */
+	virtual bool edgeExits(int i, int j) const {
+		return (const_get(i, j) != 0);
+	}
+	/*
+	 * Function: vertices
+	 * Returns the number of vertices in the adjacency structure.
+	 */
+	virtual int vertices() const {
+		return v;		
+	}
+	/*
+	 * Function: edges
+	 * Returns the number of edges in the adjacency structure.
+	 */
+	virtual int edges() const {
+		int edges = 0;
+		for (int i = 0; i < v; i++) {
+			for (int j = 0; j < v; j++) {
+				if (edgeExits(i, j)) {
+					edges++;
+				}
+			}
+		}
+
+		return edges;
+	}
+	/*
+	 * Function add:
+	 * Adds an edge between vertices i and j
+	 */
+	virtual void add(int i, int j) {
+		get(i, j) = 1;		
+	}
+	/*
+	 * Function: remove
+	 * Deleted the edge between vertices i and j
+	 */
+	virtual void remove(int i, int j) {
+		get(i, j) = 0;
+	}
+	/*
+	 * Function: degree
+	 * Returns the degree of the vertex i
+	 */
+	virtual int degree(int i) const {
+		int d = 0;
+
+		for (int j = 0; j < v; j++) {
+			if (edgeExits(i, j)) {
+				d++;
+			}
+		}	
+
+		return d;
+	}
+
+	const LinearList<int>& peak() const {
+		return matrix;
+	}
+};
+
 }
 #endif /* ifndef ADJACENCY_MATRIX */
---
+++ assignment-7/AdjacencyMatrixDriver.cpp
@@ -0,0 +1,55 @@
+#include "AdjacencyMatrix.hpp"
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::cout << "Enter the size of the list: ";
+    std::cin >> n;
+
+    cs202::AdjacencyMatrix l(n);
+
+    std::string command;
+    while (std::cin >> command) {
+        if (command == "add") {
+            int from, to;
+            std::cin >> from >> to;
+
+            l.add(from, to);
+        } else if (command == "remove") {
+            int from, to;
+            std::cin >> from >> to;
+
+            l.remove(from, to);
+        } else if (command == "degree") {
+            int v;
+            std::cin >> v;
+
+            std::cout << l.degree(v) << std::endl;
+        } else if (command == "exists") {
+            int from, to;
+            std::cin >> from >> to;
+
+            std::cout << l.edgeExits(from, to) << std::endl;
+        } else if (command == "vertices") {
+            std::cout << l.vertices() << std::endl;
+        } else if (command == "edges") {
+            std::cout << l.edges() << std::endl;
+        } else if (command == "peak") {
+            for (int i = 0; i < l.vertices(); i++) {
+                std::cout << i << ": ";
+                for (int j = 0; j < l.vertices(); j++) {
+                    if (l.edgeExits(i, j)) {
+                        std::cout << j << ", ";
+                    } 
+                }
+                std::cout << std::endl;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
