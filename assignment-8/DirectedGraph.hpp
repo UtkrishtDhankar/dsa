7f711b37695799b81bd70df61aaa33fe5ceaf091
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2017-05-10 15:52:00 +0530

    Got DirectedGraph to work with the new weighted edges

---
+++ assignment-8/DirectedGraph.hpp
@@ -0,0 +1,190 @@
+#ifndef DIRECTED_GRAPH
+#define DIRECTED_GRAPH 1
+
+#include "include/queue.hpp"
+#include "include/stack.hpp"
+
+#include "AbstractGraph.hpp"
+#include "GraphAdjacencyBase.hpp"
+#include "AdjacencyList.hpp"
+#include "AdjacencyMatrix.hpp"
+
+namespace cs202 {
+
+ /* 
+  * A class to represent a directed graph.
+  */
+class DirectedGraph : AbstractGraph {
+private:
+	GraphAdjacencyBase* g;
+	GraphMode m;
+
+public:
+	/*
+	 * Constructor: DirectedGraph
+	 *
+	 * Parameters: mode
+	 * Used to decide which representation to use
+	 * 'm' for AdjacencyMatrix
+	 * 'l' for AdjacencyList
+	 */
+	DirectedGraph(int numVertices, GraphMode mode) {
+		m = mode;
+
+		if (mode == LIST) {
+			g = new AdjacencyList(numVertices);
+		} else if (mode == MATRIX) {
+			g = new AdjacencyMatrix(numVertices);
+		} else {
+			throw std::invalid_argument("Invalid mode for DirectedGraph");
+		}
+	}
+
+	/* Destructor:
+	 * releases all resources acquired by the class
+	 */
+	virtual ~DirectedGraph() {
+
+	}
+	/*
+	 * Function: edgeExists
+	 * Returns true if an edge exists between vertices i and j, false otherwise.
+	 */
+	virtual bool edgeExits(int i, int j) const override {
+		return g->edgeExits(i, j);
+	}
+	/*
+	 * Function: edges
+	 * Returns the number of edges in the adjacency structure.
+	 */
+	virtual int edges() const override {
+		return g->edges();
+	}
+	/*
+	 * Function: vertices
+	 * Returns the number of vertices in the adjacency structure.
+	 */
+	virtual int vertices() const override {
+		return g->vertices();	
+	}
+	/*
+	 * Function add:
+	 * Adds an edge between vertices i and j
+	 */
+	virtual void add(int i, int j, int weight) override {
+		g->add(i, j, weight);
+	}
+	/*
+	 * Function: remove
+	 * Deleted the edge between vertices i and j
+	 */
+	virtual void remove(int i, int j) override {
+		g->remove(i, j);
+	}
+
+	virtual list<GraphEdge> adjacentVertices(int i) const override {
+		return g->adjacentVertices(i);
+	}
+
+	/*
+	 * Function dfs:
+	 * Does a depth first traversal of the entire graph.
+	 * Runs the given function work, with the value of each vertex.
+	 */
+	virtual LinearList<int> dfs(int source, void (*work)(int&)) override {
+		if (source >= vertices() || source < 0) {
+			throw std::invalid_argument("Source for DFS must be a valid vertex.");
+		}
+
+		stack<int> s;
+		LinearList<int> pred(vertices(), -1); // Having a predecessor of -1 means that it has no predecessor
+		LinearList<int> color(vertices(), WHITE);
+		int nodes_visited = 1;
+
+		while (nodes_visited <= vertices()) {
+			color[source] = GRAY;
+			s.push(source);
+
+			while (!s.empty()) {
+				int current = s.pop();
+				color[current] = BLACK;
+				nodes_visited++;
+
+				// Get all adjacent nodes of current into the q
+				list<GraphEdge> adjacents = g->adjacentVertices(current);
+				for (GraphEdge adjacent : adjacents) {
+					if (color[adjacent.dest] == WHITE) {
+						s.push(adjacent.dest);
+						color[adjacent.dest] = GRAY;
+						pred[adjacent.dest] = current;
+					}
+				}
+
+				work(current);
+			}
+
+			for (int i = 0; i < vertices(); i++) {
+				if (color[i] == WHITE) {
+					source = i;
+					break;
+				}
+			}
+		}
+
+		return pred;
+	}
+	/*
+	 * Function bfs:
+	 * Does a breadth first traversal of the entire graph.
+	 * Runs the given function work, with the value of each vertex.
+	 */
+	virtual LinearList<int> bfs(int source, void (*work)(int&)) override {
+		if (source >= vertices() || source < 0) {
+			throw std::invalid_argument("Source for BFS must be a valid vertex.");
+		}
+
+		queue<int> q;
+		LinearList<int> pred(vertices(), -1); // Having a predecessor of -1 means that it has no predecessor
+		LinearList<int> color(vertices(), WHITE);
+
+		color[source] = GRAY;
+		q.push(source);
+
+		while (!q.empty()) {
+			int current = q.pop();
+			color[current] = BLACK;
+
+			// Get all adjacent nodes of current into the q
+			list<GraphEdge> adjacents = g->adjacentVertices(current);
+			for (GraphEdge adjacent : adjacents) {
+				if (color[adjacent.dest] == WHITE) {
+					q.push(adjacent.dest);
+					color[adjacent.dest] = GRAY;
+					pred[adjacent.dest] = current;
+				}
+			}
+
+			work(current);
+		}
+
+		return pred;
+	}
+	/*
+	 * Function: indegree
+	 * Returns the indegree of a vertex
+	 */
+	int indegree(int i) {
+		// TODO implement this
+		return g->indegree(i);	
+	}
+	/*
+	 * Function: outdegree
+	 * Returns the outdegree of a vertex.
+	 */
+	int outdegree(int j) {
+		return g->outdegree(j);
+	}
+};
+
+}
+#endif /* ifndef DIRECTED_GRAPH */
---
+++ assignment-8/DirectedGraphDriver.cpp
@@ -0,0 +1,108 @@
+#include "DirectedGraph.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+void print_node(int& node) {
+    std::cout << node << " ";
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the size of the list: ";
+    std::cin >> n;
+
+    char ch;
+    std::cout << "Enter the mode for the graph (l/m):";
+    std::cin >> ch;
+
+    cs202::GraphMode mode;
+    if (ch == 'l' || ch == 'L') {
+        mode = cs202::LIST;
+    } else if (ch == 'm' || ch == 'M') {
+        mode = cs202::MATRIX;
+    } else {
+        throw std::invalid_argument("Mode is invalid.");
+    }
+
+    cs202::DirectedGraph l(n, mode);
+
+    std::string command;
+    while (std::cin >> command) {
+        if (command == "add") {
+            int from, to, weight;
+            std::cin >> from >> to >> weight;
+
+            l.add(from, to, weight);
+        } else if (command == "remove") {
+            int from, to;
+            std::cin >> from >> to;
+
+            l.remove(from, to);
+        } else if (command == "indegree") {
+            int v;
+            std::cin >> v;
+
+            std::cout << l.indegree(v) << std::endl;
+        } else if (command == "outdegree") {
+            int v;
+            std::cin >> v;
+
+            std::cout << l.outdegree(v) << std::endl;
+        } else if (command == "read") {
+            l = cs202::DirectedGraph(n, mode);
+
+            std::string filename;
+            std::cin >> filename;
+
+            std::ifstream f;
+            f.open(filename);
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < n; y++) {
+                    int num;
+                    f >> num;
+
+                    if (num) {
+                        l.add(x, y, num);
+                    }
+                }
+            }
+        } else if (command == "exists") {
+            int from, to;
+            std::cin >> from >> to;
+
+            std::cout << l.edgeExits(from, to) << std::endl;
+        } else if (command == "vertices") {
+            std::cout << l.vertices() << std::endl;
+        } else if (command == "edges") {
+            std::cout << l.edges() << std::endl;
+        } else if (command == "bfs") {
+            int source;
+            std::cin >> source;            
+
+            l.bfs(source, &print_node);
+            std::cout << std::endl;
+        } else if (command == "dfs") {
+            int source;
+            std::cin >> source;            
+
+            l.dfs(source, &print_node);
+            std::cout << std::endl;
+        } else if (command == "display") {
+            for (int i = 0; i < l.vertices(); i++) {
+                std::cout << i << ": ";
+
+                cs202::list<cs202::GraphEdge> adjacents = l.adjacentVertices(i);
+                for (const cs202::GraphEdge& elem : adjacents) {
+                    std::cout << "-" << elem.weight << "-> " << elem.dest << ", ";
+                }
+                std::cout << std::endl;
+            }
+        } else {
+            break;
+        }
+    }
+
+    return 0;
+}
